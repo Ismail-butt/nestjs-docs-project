3d2cb502226328577e66f55342ead377977b0064
Ismail-butt <ismailbutt89@gmail.com>
2024-01-09 19:56:35 -0800

    fix: move pagination dto to one level up

--- iluvcofee/src/coffees/coffees.service.ts
+++ iluvcofee/src/coffees/coffees.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Connection, Repository } from 'typeorm';
-import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 import { Event } from '../events/entities/event.entity';
 import { Coffee } from './entities/coffee.entity';
 import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
