a3364bdcc2847227c4a364288c4b49435858be1b
Ismail-butt <ismailbutt89@gmail.com>
2024-02-05 21:41:08 -0800

    feat: Implementing e2e Test Logic

--- iluvcofee/test/coffee/coffees.e2e-spec.ts
+++ iluvcofee/test/coffee/coffees.e2e-spec.ts
@@ -1,9 +1,16 @@
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { CoffeesModule } from '../../src/coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as request from 'supertest';
+import { CoffeesModule } from '../../src/coffees/coffees.module';
+import { CreateCoffeeDto } from '../../src/coffees/dto/create-coffee.dto/create-coffee.dto';
 
 describe('[Feature] Coffees - /coffees', () => {
+  const coffee = {
+    name: 'Coffee #1',
+    brand: 'Buddy Brew',
+    flavors: ['caramel', 'chocolate'],
+  };
   let app: INestApplication;
 
   beforeAll(async () => {
@@ -24,10 +31,35 @@ describe('[Feature] Coffees - /coffees', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
     await app.init();
   });
 
-  it.todo('Create [POST /]');
+  it('Create [POST /]', () => {
+    return request(app.getHttpServer())
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.CREATED)
+      .then(({ body }) => {
+        const expectedCoffee = expect.objectContaining({
+          ...coffee,
+          flavors: expect.arrayContaining(
+            coffee.flavors.map((name) => expect.objectContaining({ name })),
+          ),
+        });
+        expect(body).toEqual(expectedCoffee);
+      });
+  });
+
   it.todo('Get all [GET /]');
   it.todo('Get one [GET /:id]');
   it.todo('Update one [PATCH /:id]');
